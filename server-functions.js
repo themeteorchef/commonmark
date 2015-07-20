d8a8afa28aae5b5b890ba0964a13a35cb266353e
Ryan Glover <me@ryanglover.net>
2015-07-20 18:51:32 -0500

    remove leftover file

--- server-functions.js
+++
@@ -1,6 +0,0 @@
-markdownToHTML = function( string ) {
-  var reader     = new CommonMark.Parser();
-  var writer     = new CommonMark.HtmlRenderer();
-  var parsed     = reader.parse( string );
-  return writer.render( parsed );
-};
